67227d265d00dea841dd81dd9e7b741b7d39f825
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-06-13 19:05:40 -0300

    Scaffold users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/login'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   resources :suggestions
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
