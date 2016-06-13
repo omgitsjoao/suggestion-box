f158ff47de7195c2b0d29f9bc8cef8301f72d68a
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-06-13 16:23:04 -0300

    Initial commit :tada:

---
+++ app/helpers/suggestions_helper.rb
@@ -0,0 +1,9 @@
+module SuggestionsHelper
+  def short_text(text)
+    if text.length > 140
+      return text[0..140] + "..."
+    else
+      return text
+    end
+  end
+end
---
+++ app/views/suggestions/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @suggestion, :id, :content, :name, :email, :created_at, :updated_at
