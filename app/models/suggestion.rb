b9ec994d11902ff2278f08a632b0cfb1fbaa1e5e
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-06-15 00:15:31 -0300

    Add validation to suggestion model

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -1,2 +1,6 @@
 class Suggestion < ActiveRecord::Base
+  validates :content, presence: true
+  validates :name, presence: true
+  validates :email, presence: true
+  validates_format_of :email, :with =>  /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 end
